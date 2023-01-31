973f2272dbdbba876d54824faf916efd5ab5bac9
Evon Shooning <evan.schoening@gmail.com>
2023-01-31 12:14:45 -0800

    WIP for CPU architecture

--- ch5/mock-cpu/src/main.rs
+++ ch5/mock-cpu/src/main.rs
@@ -2,19 +2,25 @@
 // CHIP-8
 
 struct CPU{
-    current_operation: u16,
     // enough registers for addition
-    registers: [u8; 2], // length and type array
+    registers: [u8; 16], // length and type array
+    position_in_memory: usize, // typically referred to as program counter
+    memory: [u8; 0x1000], // represents 4096 bytes of RAM
 }
 
 impl CPU {
    fn read_opcode(&self) -> u16 {
-      self.current_operation
+      let p = self.position_in_memory;
+      let op_byte1 = self.memory[p] as u16;
+      let op_byte2 = self.memory[p + 1] as u16;
+
+      op_byte1 << 8 | op_byte2
    }
 
    fn run(&mut self) {
-      //loop{
+      loop{
          let opcode = self.read_opcode();
+         self.position_in_memory += 2;
 
          let c = ((opcode & 0xF000) >> 12) as u8;
          let x = ((opcode & 0x0F00) >> 8) as u8;
@@ -23,21 +29,33 @@ impl CPU {
 
 
          match (c, x, y, d) {
+            (0, 0, 0, 0) => {return;},
             (0x8, _, _, 0x4) => self.add_xy(x,y),
             _ => todo!("opcode {:04x}", opcode),
          }
-      //}
+      }
    }
 
    fn add_xy(&mut self, x: u8, y: u8) {
-      self.registers[x as usize] += self.registers[y as usize];
+      let arg1 = self.registers[x as usize];
+      let arg2 = self.registers[y as usize];
+
+      let (val, overflow) = arg1.overflowing_add(arg2);
+      self.registers[x as usize] = val;
+
+      if overflow {
+         self.registers[0xF] = 1;
+      } else {
+         self.registers[0xF] = 0;
+      }
    }
 }
 
 fn main() {
    let mut cpu = CPU {
-      current_operation: 0,
-      registers: [0; 2],
+      registers: [0; 16],
+      memory: [0; 4096],
+      position_in_memory: 0,
    };
    let opcode: u16 = 0x71E4;
 
@@ -57,11 +75,18 @@ fn main() {
    assert_eq!(nnn, 0x1E4);
    assert_eq!(kk, 0xE4);
 
-   cpu.current_operation = 0x8014; // op code cpu will interpret
    cpu.registers[0] = 5;
    cpu.registers[1] = 10;
+   cpu.registers[2] = 10;
+   cpu.registers[3] = 10;
+
+   let mem = &mut cpu.memory;
+   mem[0] = 0x80; mem[1] = 0x14;
+   mem[2] = 0x80; mem[3] = 0x24; // different registers to add 
+   mem[4] = 0x80; mem[5] = 0x34;
+
 
    cpu.run();
-   assert_eq!(cpu.registers[0], 15);
+   assert_eq!(cpu.registers[0], 35);
    println!("5 + 10 = {}", cpu.registers[0]);
 }
