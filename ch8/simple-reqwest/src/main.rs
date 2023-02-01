c14999fb55c69e8a427b429705e8519d80a69a89
Evon Shooning <evan.schoening@gmail.com>
2023-02-01 13:20:36 -0800

    working tcp arrangement

---
+++ ch8/simple-reqwest/src/main.rs
@@ -0,0 +1,13 @@
+use std::error::Error;
+
+use reqwest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "http://www.rustinaction.com/";
+    let mut response = reqwest::get(url).await?;
+
+    let content = response.text().await?;
+    println!("{}", content);
+    Ok(())
+}
